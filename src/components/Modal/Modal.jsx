3b5cbbb8579f73b80e21755fb7516863d73b12ad
Natatashkin <cs070283sng@gmail.com>
2022-06-09 03:01:15 +0300

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,17 @@
 import './Modal.scss';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#root-modal');
 
 const Modal = ({ children, open, onClose }) => {
+  // useEffect(() => {
+
+  //   document.addEventListener('keydown', onEscKeyPress);
+
+  //   return () => document.removeEventListener('keydown', onEscKeyPress);
+  // }, []);
+
   return open
     ? createPortal(
         <div className="dropbox" onClick={onClose}>
